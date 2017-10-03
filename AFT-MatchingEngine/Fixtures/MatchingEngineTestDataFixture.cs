633cbf0f774a0a1b6e9d9427fe01d76fd0ea6069
Galin <galin.dobchev@primeholding.com>
2017-10-03 17:15:18 +0300

    initialized matching engine tests project

---
+++ AFT-MatchingEngine/Fixtures/MatchingEngineTestDataFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XUnitTestCommon;
+
+namespace AFTMatchingEngine.Fixtures
+{
+    public class MatchingEngineTestDataFixture : IDisposable
+    {
+        public MatchingEngineConsumer Consumer;
+        private ConfigBuilder _configBuilder;
+
+        public MatchingEngineTestDataFixture()
+        {
+            this._configBuilder = new ConfigBuilder("MatchingEngine");
+
+            if (Int32.TryParse(_configBuilder.Config["Port"], out int port))
+            {
+                Consumer = new MatchingEngineConsumer(_configBuilder.Config["BaseUrl"], port);
+                Consumer.Connect();
+            }
+            else
+            {
+                throw new FormatException();
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ AFT-MatchingEngine/Tests/MatchingEngineServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AFTMatchingEngine.Fixtures;
+using Xunit;
+using RestSharp;
+using XUnitTestCommon;
+using Lykke.MatchingEngine.Connector.Abstractions.Models;
+
+namespace AFTMatchingEngine
+{
+    [Trait("Category", "FullRegression")]
+    [Trait("Category", "MatchingEngine")]
+    public class MatchingEngineServiceTests : IClassFixture<MatchingEngineTestDataFixture>
+    {
+        private MatchingEngineTestDataFixture fixture;
+
+        public MatchingEngineServiceTests(MatchingEngineTestDataFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public async void CashInOut()
+        {
+            Assert.NotNull(fixture.Consumer.Client);
+            Assert.True(fixture.Consumer.Client.IsConnected);
+            Guid newId = Guid.NewGuid();
+
+            MeResponseModel test = await fixture.Consumer.Client.CashInOutAsync(newId.ToString(), "test", "test", 0);
+            Assert.True(test.Status == MeStatusCodes.Ok);
+        }
+
+    }
+}
