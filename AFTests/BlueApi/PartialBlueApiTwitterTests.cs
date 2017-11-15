5a8666112dee00a2de059363167fac20ab4b9e10
Todor Ivanov <todor.ivanov@primeholding.com>
2017-11-15 11:56:06 +0200

    Add Twitter Endpoint tests

---
+++ AFTests/BlueApi/PartialBlueApiTwitterTests.cs
@@ -0,0 +1,53 @@
+﻿using BlueApiData.DTOs;
+using BlueApiData.Fixtures;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using XUnitTestCommon;
+using XUnitTestCommon.Utils;
+
+namespace AFTests.BlueApi
+{
+    [Trait("Category", "FullRegression")]
+    [Trait("Category", "BlueApiService")]
+    public partial class BlueApiTests : IClassFixture<BlueApiTestDataFixture>
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        [Trait("Category", "Twitter")]
+        [Trait("Category", "TwitterPost")]
+        public async void GetTweets()
+        {
+            string url = _fixture.ApiEndpointNames["Twitter"];
+            int pageSize = Helpers.Random.Next(0, 101); // from 0 to 100 records on a page
+            var body = new TwitterSearchDTO()
+            {
+                SearchQuery = _fixture.TwitterSearchQuery,
+                MaxResult = 100,
+                UntilDate = _fixture.TwitterSearchUntilDate, // hardcoded
+                AccountEmail = _fixture.AccountEmail,
+                PageNumber = 1,
+                PageSize = pageSize
+            };
+            var response = await _fixture.Consumer.ExecuteRequest(url, Helpers.EmptyDictionary, JsonUtils.SerializeObject(body), RestSharp.Method.POST);
+
+            // checks if the response is 200 OK
+            Assert.True(response.Status == System.Net.HttpStatusCode.OK);
+
+            List<TweetDTO> tweets = JsonUtils.DeserializeJson<List<TweetDTO>>(response.ResponseJson);
+
+            // checks if the right amout of tweets is returned on a single page
+            Assert.True(tweets.Count == pageSize);
+
+            bool containsSearchQuery = true;
+            tweets.ForEach(tweet =>
+            {
+                if (!tweet.Title.ToLower().Contains(_fixture.TwitterSearchQuery))
+                    containsSearchQuery = false;
+            });
+
+            //checks if all tweets contain the search query in their title
+            Assert.True(containsSearchQuery);
+        }
+    }
+}
---
+++ XUnitTestData/Domains/BlueApi/ITwitterEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestData.Domains.BlueApi
+{
+    interface ITwitterEntity: IDictionaryItem
+    {
+        bool IsExtendedSearch { get; set; }
+        string AccountEmail { get; set; }
+        string SearchQuery { get; set; }
+        int MaxResult { get; set; }
+        DateTime UntilDate { get; set; }
+        int PageSize { get; set; }
+        int PageNumber { get; set; }
+    }
+}
