d8fd14fe25c16f091d23071b5c49997424d051d4
Miladin Joksic <miladin.joksic@primeholding.com>
2017-11-20 11:43:26 +0100

    Added request gift coins endpoint tests

--- AFTests/BlueApi/PartialBlueApiReferralLinksTests.cs
+++ AFTests/BlueApi/PartialBlueApiReferralLinksTests.cs
@@ -47,5 +47,61 @@ namespace AFTests.BlueApi
             Assert.True(Guid.TryParse(parsedResponse.RefLinkId, out refLinkId));
             Assert.True(refLinkId != Guid.Empty);
         }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("ReferralLinks")]
+        [Category("RequestGiftCoinsLinkForNonExistingClient")]
+        public async Task RequestGiftCoinsLinkForNonExistingClient()
+        {
+            var url = $"{ApiPaths.REFERRAL_LINKS_BASE_PATH}/request/giftCoinslLink";
+            var body = JsonUtils.SerializeObject(new RequestGiftCoinsLinkRequestDto { SenderClientId = "123" });
+
+            var response = await _fixture.Consumer.ExecuteRequest(url, Helpers.EmptyDictionary, body, Method.POST);
+
+            Assert.True(response.Status == HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("ReferralLinks")]
+        [Category("RequestGiftCoinsLinkForInvalidAsset")]
+        public async Task RequestGiftCoinsLinkForInvalidAsset()
+        {
+            var url = $"{ApiPaths.REFERRAL_LINKS_BASE_PATH}/request/giftCoinslLink";
+            var body = JsonUtils.SerializeObject(new RequestGiftCoinsLinkRequestDto { Asset = "123" });
+
+            var response = await _fixture.Consumer.ExecuteRequest(url, Helpers.EmptyDictionary, body, Method.POST);
+
+            Assert.True(response.Status == HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("ReferralLinks")]
+        [Category("RequestGiftCoinsLinkForInvalidAmount")]
+        public async Task RequestGiftCoinsLinkForInvalidAmount()
+        {
+            var url = $"{ApiPaths.REFERRAL_LINKS_BASE_PATH}/request/giftCoinslLink";
+            var body = JsonUtils.SerializeObject(new RequestGiftCoinsLinkRequestDto { Amount = 123 });
+
+            var response = await _fixture.Consumer.ExecuteRequest(url, Helpers.EmptyDictionary, body, Method.POST);
+
+            Assert.True(response.Status == HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("ReferralLinks")]
+        [Category("RequestGiftCoinsLinkForInvalidAllParams")]
+        public async Task RequestGiftCoinsLinkForInvalidAllParams()
+        {
+            var url = $"{ApiPaths.REFERRAL_LINKS_BASE_PATH}/request/giftCoinslLink";
+            var body = JsonUtils.SerializeObject(new RequestGiftCoinsLinkRequestDto { Asset = "123", Amount = 123, SenderClientId = "123"});
+
+            var response = await _fixture.Consumer.ExecuteRequest(url, Helpers.EmptyDictionary, body, Method.POST);
+
+            Assert.True(response.Status == HttpStatusCode.BadRequest);
+        }
     }
 }
---
+++ BlueApiData/DTOs/RequestGiftCoinsLinkRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueApiData.DTOs
+{
+    public class RequestGiftCoinsLinkRequestDto
+    {
+        public string SenderClientId { get; set; }
+        public string Asset { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
