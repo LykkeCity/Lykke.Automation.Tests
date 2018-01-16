a534f1e1acb28beb28155d4b853101cb09d89461
Fil Mihaylov <filmihaylov1989@gmail.com>
2018-01-16 13:23:27 +0200

    change binary values to empty for now

--- AlgoStoreData.cs/HelpersAlgoStore/BinaryAlgoFileType.cs
+++ AlgoStoreData.cs/HelpersAlgoStore/BinaryAlgoFileType.cs
@@ -6,9 +6,9 @@ namespace AlgoStoreData.HelpersAlgoStore
 {
     public static class BinaryAlgoFileType
     {
-        public static string JAR = ".jar";
-        public static string NET = ".exe";
-        public static string PY = ".py";
-        public static string STRING = ".java";
+        public static string JAR = "";
+        public static string NET = "";
+        public static string PY = "";
+        public static string STRING = "";
     }
 }
