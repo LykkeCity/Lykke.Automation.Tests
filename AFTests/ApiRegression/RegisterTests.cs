e272af117270e2a3501b27d619e15be83937ef5e
Andrey Gurenkov <meetold@gmail.com>
2018-01-30 22:04:55 +0300

    Api Regression: few steps in Register Test Api validation on error added

--- AFTests/ApiRegression/RegisterTests.cs
+++ AFTests/ApiRegression/RegisterTests.cs
@@ -19,28 +19,33 @@ namespace AFTests.ApiRegression
             string clientInfo = "iPhone; Model:6 Plus; Os:9.3.5; Screen:414x736";
             string hint = "qwe";
             string password = Guid.NewGuid().ToString("N");
+            string token = null;
 
             //STEP 1
-            var getClientState = walletApi.ClientState.GetClientState(email, null);
-            Assert.Multiple(() =>
-            {
-                getClientState.Validate.StatusCode(HttpStatusCode.OK);
-                var getClientStateData = getClientState.GetResponseObject();
-                Assert.That(getClientStateData.Error, Is.Null);
-                Assert.That(getClientStateData.Result.IsRegistered, Is.False);
-            });
+            var getClientState = walletApi.ClientState
+                .GetClientState(email, null)
+                .Validate.StatusCode(HttpStatusCode.OK)
+                .Validate.NoApiError();
+            Assert.That(getClientState.GetResponseObject().Result
+                    .IsRegistered, Is.False);
+
             //STEP 2
             var postEmailVerification = walletApi.EmailVerification
                 .PostEmailVerification(new PostEmailModel()
                 {
                     Email = email
-                }).Validate.StatusCode(HttpStatusCode.OK);
+                })
+                .Validate.StatusCode(HttpStatusCode.OK)
+                .Validate.NoApiError();
             Assert.That(postEmailVerification.GetResponseObject().Error, Is.Null);
+
             //STEP 3
             var getEmailVerification = walletApi.EmailVerification
                 .GetEmailVerification(email, code, null)
-                .Validate.StatusCode(HttpStatusCode.OK);
+                .Validate.StatusCode(HttpStatusCode.OK)
+                .Validate.NoApiError();
             Assert.That(getEmailVerification.GetResponseObject().Result.Passed, Is.True);
+
             //STEP 4
             var postRegistration = walletApi.Registration.PostRegistrationResponse(new AccountRegistrationModel()
             {
@@ -48,15 +53,39 @@ namespace AFTests.ApiRegression
                 Email = email,
                 Hint = hint,
                 Password = password
-            }).Validate.StatusCode(HttpStatusCode.OK);
+            })
+            .Validate.StatusCode(HttpStatusCode.OK)
+            .Validate.NoApiError();
             Assert.Multiple(() =>
             {
-                var account = postRegistration.GetResponseObject().Result;
-                Assert.That(postRegistration.GetResponseObject().Error, Is.Null);
-                //Assert.That(account.Token, Is.Not.Null);
-                //Assert.That(account.NotificationsId, Is.Not.Null);
-                Assert.That(account.PersonalData?.Email, Is.EqualTo(email));
+                var postRegistrationData = postRegistration.GetResponseObject();
+                Assert.That(postRegistrationData.Result.PersonalData?.Email, Is.EqualTo(email));
+                Assert.That(postRegistrationData.Result.Token, Is.Not.Null.And.Not.Empty);
             });
+            token = postRegistration.GetResponseObject().Result.Token;
+
+            //STEP 5
+            getClientState = walletApi.ClientState
+                .GetClientState(email, null)
+                .Validate.StatusCode(HttpStatusCode.OK)
+                .Validate.NoApiError();
+            Assert.Multiple(() =>
+            {
+                var getClientStateData = getClientState.GetResponseObject();
+                Assert.That(getClientStateData.Result.IsRegistered, Is.True);
+                Assert.That(getClientStateData.Result.IsPwdHashed, Is.True);
+            });
+
+            //STEP 6
+            var getPersonalData = walletApi.PersonalData
+                .GetPersonalDataResponse(token)
+                .Validate.StatusCode(HttpStatusCode.OK)
+                .Validate.NoApiError();
+            Assert.That(getPersonalData.GetResponseObject().Result
+                    .Email, Is.EqualTo(email));
+
+            //STEP 7
+            //var postClientFullName = walletApi.ClientFullName.PostClientFullName
         }
     }
 }
