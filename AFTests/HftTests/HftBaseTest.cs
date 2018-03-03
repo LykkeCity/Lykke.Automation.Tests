f8aa3cbd268f3810b7c3789fb52ba27479605390
Pavel Yasonau <megomozg@gmail.com>
2018-03-03 21:40:02 +0300

    + IsAlive

--- AFTests/HftTests/HftBaseTest.cs
+++ AFTests/HftTests/HftBaseTest.cs
@@ -1,8 +1,10 @@
 ﻿using HFT;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using XUnitTestCommon.Tests;
+using XUnitTestCommon.TestsCore;
 
 namespace AFTests.HftTests
 {
@@ -11,5 +13,21 @@ namespace AFTests.HftTests
         protected const string ApiKey = "6eba78a1-1594-462f-9b28-3822baf9397c";
 
         protected Hft hft = new Hft();
+
+
+        [OneTimeTearDown]
+        public void SetProperty()
+        {
+            AllurePropertiesBuilder.Instance.AddPropertyPair("Date", DateTime.Now.ToString());
+            try
+            {
+                var isAlive = hft.IsAlive.GetIsAlive().GetResponseObject();
+                AllurePropertiesBuilder.Instance.AddPropertyPair("Env", isAlive.Env);
+                AllurePropertiesBuilder.Instance.AddPropertyPair("Version", isAlive.Version);
+            }
+            catch (Exception) { /*do nothing*/}
+
+            new Allure2Report().CreateEnvFile();
+        }
     }
 }
---
+++ AFTests/HftTests/IsAlive.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AFTests.HftTests
+{
+    class IsAlive
+    {
+        public class GetIsAlive : HftBaseTest
+        {
+            [Test]
+            [Category("HFT")]
+            public void GetIsAliveTest()
+            {
+                var response = hft.IsAlive.GetIsAlive();
+                response.Validate.StatusCode(HttpStatusCode.OK);
+                Assert.That(response.GetResponseObject().Version, Is.Not.Null.Or.Empty, "Version is null or empty");
+            }
+        }
+    }
+}
