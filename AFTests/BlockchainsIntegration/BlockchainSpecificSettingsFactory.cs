975538bd5b3c1acf59786dcbd26ceef11773dcea
Pavel Yasonau <megomozg@gmail.com>
2018-03-21 23:05:50 +0300

    - wallet single use address

--- AFTests/BlockchainsIntegration/BlockchainSpecificSettingsFactory.cs
+++ AFTests/BlockchainsIntegration/BlockchainSpecificSettingsFactory.cs
@@ -75,8 +75,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletAddress = "msvNWBpFNDQ6JxiEcTFU3xXbSnDir4EqCk";
                 DepositWalletKey = "cRTB3eAajJchgNuybhH5SwC9L5PFoTwxXBjgB8vRNJeJ4EpcXmAP";
-                WalletSingleUse = "mvErcbPuL4T4kxbJYejk6xLbv8pfBiiSPu";
-                WalletSingleUseKey = "cNn38kw6LSfAS6WvJJbFTqWRewa1GgfwczftXrBcyAmygM1V7qKr";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "LTC";
             }
@@ -91,8 +89,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletAddress = "yUDQmubM2HtBFmkvbSK1rER1t57M5Mcvng";
                 DepositWalletKey = "cPX3K2xfuzoakmXMaJG5HrdFKuACxegcax5eq55SMHJ8YxvmttZz";
-                WalletSingleUse = "yiH2MLsx6bVZFgQ9qQNj5QeetGft8xDacC";
-                WalletSingleUseKey = "cQinitZ5SkZdARZPuXicFgGkKepWcjpB5fTx5WKHdvkMTdnB1yrq";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "DASH";
             }
@@ -107,8 +103,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletAddress = "tmQjumT79zunETQgFxNTEMUKz8D841fMCf1";
                 DepositWalletKey = "cRPW3spyP9riDJWniNpcbDkiBjpLrhneSh2qTs3uSZUbm4HZLEyB";
-                WalletSingleUse = "tmCiRyHFZYeRyXV1wqyiqZad2Zf9ifdR9H5";
-                WalletSingleUseKey = "cVhrfsddvvhJhEEoqocSpdwZjJrTdQmHdWRKyvSqhyYvBDFD4die";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "ZEC";
             }
@@ -123,8 +117,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletKey = "snfFfwhb7tobJcamWtH6GhvvhM6pi";
                 DepositWalletAddress = "rwAC8DbqubkRcYKuQQaqSBYppHD4JU84Pa";
-                WalletSingleUse = "rwAC8DbqubkRcYKuQQaqSBYppHD4JU84Pa";
-                WalletSingleUseKey = "snfFfwhb7tobJcamWtH6GhvvhM6pi";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "XRP";
             }
@@ -139,8 +131,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletKey = "SA7W5C7CQOOJF2AGFL2B2LC7VM6WMAONYTAWVITGAKOJM757YDG4VOQG";
                 DepositWalletAddress = "GDGZG75SP7UVW6RRDNMFGCFFC5D5RZTXLAWYWUKYCHJ6SBJ2FTXFLXHA";
-                WalletSingleUse = "GDGZG75SP7UVW6RRDNMFGCFFC5D5RZTXLAWYWUKYCHJ6SBJ2FTXFLXHA";
-                WalletSingleUseKey = "SA7W5C7CQOOJF2AGFL2B2LC7VM6WMAONYTAWVITGAKOJM757YDG4VOQG";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "XLM";
             }
@@ -155,8 +145,6 @@ namespace AFTests.BlockchainsIntegration
                 WalletsUrl = null;
                 DepositWalletKey = "SA7W5C7CQOOJF2AGFL2B2LC7VM6WMAONYTAWVITGAKOJM757YDG4VOQG";
                 DepositWalletAddress = "GDGZG75SP7UVW6RRDNMFGCFFC5D5RZTXLAWYWUKYCHJ6SBJ2FTXFLXHA";
-                WalletSingleUse = "GDGZG75SP7UVW6RRDNMFGCFFC5D5RZTXLAWYWUKYCHJ6SBJ2FTXFLXHA";
-                WalletSingleUseKey = "SA7W5C7CQOOJF2AGFL2B2LC7VM6WMAONYTAWVITGAKOJM757YDG4VOQG";
                 ClientId = "b623b171-a307-4485-897c-f3a70b763217";
                 AssetId = "XLM";
             }
@@ -176,8 +164,6 @@ namespace AFTests.BlockchainsIntegration
                 HotWalletKey = "keep_keys_private";
                 ExternalWalletAddress = "1.2.20407:::06eea045-43ee-4cca-a19d-1356abc2b70e";
                 ExternalWalletKey = "keep_keys_private";
-                WalletSingleUse = "";
-                WalletSingleUseKey = "";
                 ClientId = "";
                 AssetId = "1.3.0";
             }
@@ -203,8 +189,6 @@ namespace AFTests.BlockchainsIntegration
         public string WalletsUrl { get; set; }
         public string DepositWalletAddress { get; set; }
         public string DepositWalletKey { get; set; }
-        public string WalletSingleUse { get; set; }
-        public string WalletSingleUseKey { get; set; }
         public string ClientId { get; set; }
         public string AssetId { get; set; }
         public string ExternalWalletAddress { get; set; }
