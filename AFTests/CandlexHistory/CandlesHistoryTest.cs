9829b447a970ee682523d7f22e1c42c2a8c29c3c
Pavel Yasonau <megomozg@gmail.com>
2018-04-27 00:06:45 +0300

    + Candles resources

---
+++ AFTests/CandlexHistory/CandlesHistoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AFTests.PrivateApiTests;
+using Lykke.Client.AutorestClient.Models;
+using NUnit.Framework;
+using XUnitTestCommon.Tests;
+
+namespace AFTests.CandlexHistory
+{
+    class CandlesHistoryTest
+    {
+        public class CandleHistory : PrivateApiBaseTest
+        {
+            [Test]
+            [Category("CandleHistory")]
+            public void CandleHistoryTest()
+            {
+                var list = lykkeApi.CandleHistory.GetAvailableAssetsPairs().GetResponseObject();
+
+
+                var request2 = lykkeApi.CandleHistory.PostCandlesHistoryBatch(new GetCandlesHistoryBatchRequest() {
+                    AssetPairs = new List<string>() { list[0]},
+                    FromMoment = DateTime.Now.AddDays(-5),
+                    TimeInterval = CandleTimeInterval.Hour,
+                    ToMoment = DateTime.Now,
+                    PriceType = CandlePriceType.Mid
+                });
+
+                var request3 = lykkeApi.CandleHistory.GetCandleHistory(list[0], CandlePriceType.Trades, CandleTimeInterval.Min30, DateTime.Now.AddDays(-7), DateTime.Now.AddDays(-3));
+            }
+        }
+    }
+}
---
+++ PrivateServices/Resources/CandlesHistory/CandlesHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Client.AutorestClient.Models;
+using RestSharp;
+using XUnitTestCommon.RestRequests;
+using XUnitTestCommon.RestRequests.Interfaces;
+
+namespace PrivateServices.Resources.CandlesHistory
+{
+    public class CandlesHistory
+    {
+        private IRequestBuilder Request = Requests.For("http://candles-history.lykke-service.svc.cluster.local/api");
+
+        public IResponse<List<string>> GetAvailableAssetsPairs()
+        {
+            return Request.Get("/CandlesHistory/availableAssetPairs").Build().Execute<List<string>>();
+        }
+
+        public IResponse PostCandlesHistoryBatch(GetCandlesHistoryBatchRequest model)
+        {
+            return Request.Post("/CandlesHistory/batch").AddJsonBody(model).Build().Execute();
+        }
+
+        public IResponse<CandlesHistoryResponseModel> GetCandleHistory(string assetPairId, CandlePriceType priceType, CandleTimeInterval timeInterval, DateTime fromMoment, DateTime toMoment)
+        {
+            return Request.Get($"/CandlesHistory/{assetPairId}/{priceType}/{timeInterval}/{fromMoment.ToString("s")}/{toMoment.ToString("s")}").Build().Execute<CandlesHistoryResponseModel>();
+        }
+
+        public IResponse<IsAliveResponse> GetIsAlive()
+        {
+            return Request.Get("/IsAlive").Build().Execute<IsAliveResponse>();
+        }
+    }
+}
