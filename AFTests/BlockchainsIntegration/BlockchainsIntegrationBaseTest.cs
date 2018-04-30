86241849f90978ecbb8bea7b76be3f0fbd2ed44c
Pavel Yasonau <megomozg@gmail.com>
2018-04-30 15:44:31 +0300

    + Set env properties with tests result

--- AFTests/BlockchainsIntegration/BlockchainsIntegrationBaseTest.cs
+++ AFTests/BlockchainsIntegration/BlockchainsIntegrationBaseTest.cs
@@ -33,7 +33,7 @@ namespace AFTests.BlockchainsIntegrationTests
 
        protected static string SpecificBlockchain()
        {
-            return Environment.GetEnvironmentVariable("BlockchainIntegration") ?? "ripple"; //"RaiBlocks";//"bitshares";// "stellar-v2";//"Zcash"; //"Ripple";// "Dash"; "Litecoin";
+            return Environment.GetEnvironmentVariable("BlockchainIntegration") ?? "bitshares"; //"RaiBlocks";//"bitshares";// "stellar-v2";//"Zcash"; //"Ripple";// "Dash"; "Litecoin";
         }
 
         protected static string BlockchainApi { get { return _currentSettings.Value.BlockchainApi; } }
@@ -213,5 +213,19 @@ namespace AFTests.BlockchainsIntegrationTests
                     break;
             }
         }
-    }  
+    }
+
+    // move that and get work into BaseTest
+    [SetUpFixture]
+    public class HelperClass
+    {
+        [OneTimeTearDown]
+        public void AfterTests()
+        {
+            var context = TestContext.CurrentContext;
+            Environment.SetEnvironmentVariable("failedTests", context.Result.FailCount.ToString());
+            Environment.SetEnvironmentVariable("passedTests", context.Result.PassCount.ToString());
+            Environment.SetEnvironmentVariable("skippedTests", context.Result.SkipCount.ToString());
+        }
+    }
 }
