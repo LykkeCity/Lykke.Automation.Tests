7feed45c4bf1f82101c83ff94b2ffc0d1efb44b8
georgi.ivanov <georgi.ivanov@primeholding.com>
2018-06-01 18:43:52 +0300

    Update creation of algo process

---
+++ AlgoStoreData.cs/HelpersAlgoStore/Base64Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoStoreData.HelpersAlgoStore
+{
+    public class Base64Helpers
+    {
+        public static string EncodeToBase64(string stringToConvert)
+        {
+            byte[] encodedBytes = Encoding.UTF8.GetBytes(stringToConvert);
+            return Convert.ToBase64String(encodedBytes);
+        }
+
+        public static string DecodeBase64String(string base64String)
+        {
+            byte[] decodedBytes = Convert.FromBase64String(base64String);
+            return Encoding.UTF8.GetString(decodedBytes);
+        }
+    }
+}
