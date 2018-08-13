a4ede9d05191f0bc0602f8ce517d63162bfcb86c
Pavel Yasonau <megomozg@gmail.com>
2018-08-13 21:02:33 +0300

    + Cypress test project

---
+++ Cypress/CypressProj/CypressProj/first.spec.js
@@ -0,0 +1,22 @@
+﻿/// <reference types="Cypress" />
+
+context('Lykke first test', () => {
+    beforeEach(() => {
+        cy.visit('https://lykke.com')
+    })
+
+
+    it('sample', () => {
+        // https://on.cypress.io/type
+        expect(3).to.equals(3)
+
+        cy.get('.header_login__title').click()
+    })
+
+    it('sample', () => {
+        // https://on.cypress.io/type
+        expect(3).to.equals(3)
+
+        cy.get('.header_login__title').click()
+    })
+})
