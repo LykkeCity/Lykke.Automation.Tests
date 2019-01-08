60127a05cc272d6f2c58ba47eecade0fb3d41da4
Yury Glushkov <mail@glushkov.us>
2019-01-08 14:16:31 +0300

    Ethereum Classic settings added

--- AFTests/BlockchainsIntegration/BlockchainSpecificSettingsFactory.cs
+++ AFTests/BlockchainsIntegration/BlockchainSpecificSettingsFactory.cs
@@ -93,6 +93,9 @@ namespace AFTests.BlockchainsIntegration
             if (blockchain == "ethereum")
                 _settings = new EthereumSettings();
 
+            if (blockchain == "ethereumclassic")
+                _settings = new EthereumClassicSettings();
+            
             if (blockchain == "kin")
                 _settings = new KinSettings();
 
@@ -430,6 +433,24 @@ namespace AFTests.BlockchainsIntegration
                 HotWallet = "0xec31eafd3818fbd7c14a423ee09090351d864ba1";
                 HotWalletKey = "0x2df14f93b65c7fbe6623aef1f09183d704f7f24ad2d615eaecc9fd44f725b269";
                 AssetId = "ETH";
+                ExternalWalletAddress = "0x085e36cc33743be10246013d11b8fc1919a242db";
+                ExternalWalletKey = "0x2c0b8b8f59e2ed253bb45c0974852cae982416823eb4f6775192f167e7b15825";
+                BuildSignBroadcastEWDW = 1;
+                AssetAccuracy = 15;
+            }
+        }
+        
+        class EthereumClassicSettings : BlockchainSpecificModel
+        {
+            public EthereumClassicSettings()
+            {
+                BlockchainIntegration = "EthereumClassic";
+                BlockchainApi = "http://etc-api.bcn-autotests.svc.cluster.local/api";
+                BlockchainSign = "http://etc-signservice.bcn-autotests.svc.cluster.local/api";
+                BlockchainMiningTime = 5;
+                HotWallet = "0xec31eafd3818fbd7c14a423ee09090351d864ba1";
+                HotWalletKey = "0x2df14f93b65c7fbe6623aef1f09183d704f7f24ad2d615eaecc9fd44f725b269";
+                AssetId = "ETC";
                 ExternalWalletAddress = "0x085e36cc33743be10246013d11b8fc1919a242db";
                 ExternalWalletKey = "0x2c0b8b8f59e2ed253bb45c0974852cae982416823eb4f6775192f167e7b15825";
                 BuildSignBroadcastEWDW = 1;
